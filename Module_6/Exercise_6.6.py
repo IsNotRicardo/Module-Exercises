94ea2bfbdc01edfe38c61062557cff18e800b5d0
IsNotRicardo <desousa.itsnotricardo@gmail.com>
2023-09-14 22:21:37 +0300

    Fixed Module 6.6

--- Module_6/Exercise_6.6.py
+++ Module_6/Exercise_6.6.py
@@ -2,23 +2,18 @@ import math
 
 
 def unit_price(length, cost):
-    """Issue: The function is inverting the parameters"""
-    """prints are for debugging"""
-    print(length / 100)
-    print(20 / (math.pi * (35 / 200) ** 2))
-    print(cost / (math.pi * (length / 200) ** 2))
     return cost / (math.pi * (length / 200) ** 2)
 
 
 def main():
-    diameter = price = value = list(range(2))
+    diameter, price, value = list(range(2)), list(range(2)), list(range(2))
 
     for i in range(2):
-        diameter[i] = float(input(f"Insert the diameter of pizza Nº{i + 1} (cm): "))
+        diameter[i] = float(input(f"\nInsert the diameter of pizza Nº{i + 1} (cm): "))
         price[i] = float(input(f"Insert the price of pizza Nº{i + 1} (€): "))
-        value[i] = unit_price(length=diameter[i], cost=price[i])
+        value[i] = unit_price(diameter[i], price[i])
 
-    print(f"The pizza with the best value is: Nº{value.index(min(value)) + 1}, costing {min(value):.2f} €/m2\n"
+    print(f"\nThe pizza with the best value is: Nº{value.index(min(value)) + 1}, costing {min(value):.2f} €/m2\n"
           f"The other pizza is: Nº{value.index(max(value)) + 1}, costing {max(value):.2f} €/m2")
 
 
